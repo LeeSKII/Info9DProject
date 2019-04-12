4cdcadd52198dbcfdd8ddcf79b1d5cf2a5b36075
ski lee <540470452@qq.com>
2019-04-12 10:06:44 +0800

    添加项目文件。

---
+++ Info9DProject/Start.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace Info9DProject
+{
+    [PluginAttribute("Info9D", "CIEPMIP", DisplayName = "9D信息", ToolTip = "BIM模型9D信息管理")]
+    public class Start:AddInPlugin
+    {
+        public override int Execute(params string[] parameters)
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.Show();
+
+            return 0;
+        }
+    }
+}
