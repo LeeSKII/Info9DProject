efdb5926d3babf371fbc7dc3cdd6773d7e958ad8
ski lee <540470452@qq.com>
2019-04-12 16:02:44 +0800

    待完成修改、删除

--- Info9DProject/MainForm.Designer.cs
+++ Info9DProject/MainForm.Designer.cs
@@ -28,19 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.DGVTotal = new System.Windows.Forms.DataGridView();
+            this.TCLTotal = new System.Windows.Forms.TabControl();
+            this.QualityTP = new System.Windows.Forms.TabPage();
+            this.TBDescribeQuality = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SafetyTP = new System.Windows.Forms.TabPage();
+            this.TBDescribeSafety = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BUTAdd = new System.Windows.Forms.Button();
+            this.BUTShow = new System.Windows.Forms.Button();
+            this.IDofDGVTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NAMEofDGVTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BUTFocus = new System.Windows.Forms.Button();
+            this.BUTQuery = new System.Windows.Forms.Button();
+            this.BUTChange = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVTotal)).BeginInit();
+            this.TCLTotal.SuspendLayout();
+            this.QualityTP.SuspendLayout();
+            this.SafetyTP.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 57);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.DGVTotal);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.TCLTotal);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 393);
+            this.splitContainer1.SplitterDistance = 190;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // DGVTotal
+            // 
+            this.DGVTotal.AllowUserToAddRows = false;
+            this.DGVTotal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVTotal.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.DGVTotal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVTotal.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IDofDGVTotal,
+            this.NAMEofDGVTotal});
+            this.DGVTotal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVTotal.Location = new System.Drawing.Point(0, 0);
+            this.DGVTotal.Name = "DGVTotal";
+            this.DGVTotal.RowTemplate.Height = 23;
+            this.DGVTotal.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGVTotal.Size = new System.Drawing.Size(800, 190);
+            this.DGVTotal.TabIndex = 0;
+            this.DGVTotal.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVTotal_CellClick);
+            // 
+            // TCLTotal
+            // 
+            this.TCLTotal.Controls.Add(this.QualityTP);
+            this.TCLTotal.Controls.Add(this.SafetyTP);
+            this.TCLTotal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TCLTotal.Location = new System.Drawing.Point(0, 0);
+            this.TCLTotal.Name = "TCLTotal";
+            this.TCLTotal.SelectedIndex = 0;
+            this.TCLTotal.Size = new System.Drawing.Size(800, 199);
+            this.TCLTotal.TabIndex = 0;
+            // 
+            // QualityTP
+            // 
+            this.QualityTP.Controls.Add(this.TBDescribeQuality);
+            this.QualityTP.Controls.Add(this.label1);
+            this.QualityTP.Location = new System.Drawing.Point(4, 22);
+            this.QualityTP.Name = "QualityTP";
+            this.QualityTP.Padding = new System.Windows.Forms.Padding(3);
+            this.QualityTP.Size = new System.Drawing.Size(792, 173);
+            this.QualityTP.TabIndex = 0;
+            this.QualityTP.Text = "质量";
+            this.QualityTP.UseVisualStyleBackColor = true;
+            // 
+            // TBDescribeQuality
+            // 
+            this.TBDescribeQuality.Location = new System.Drawing.Point(85, 25);
+            this.TBDescribeQuality.Name = "TBDescribeQuality";
+            this.TBDescribeQuality.Size = new System.Drawing.Size(165, 21);
+            this.TBDescribeQuality.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "说明：";
+            // 
+            // SafetyTP
+            // 
+            this.SafetyTP.Controls.Add(this.TBDescribeSafety);
+            this.SafetyTP.Controls.Add(this.label2);
+            this.SafetyTP.Location = new System.Drawing.Point(4, 22);
+            this.SafetyTP.Name = "SafetyTP";
+            this.SafetyTP.Padding = new System.Windows.Forms.Padding(3);
+            this.SafetyTP.Size = new System.Drawing.Size(792, 173);
+            this.SafetyTP.TabIndex = 1;
+            this.SafetyTP.Text = "安全";
+            this.SafetyTP.UseVisualStyleBackColor = true;
+            // 
+            // TBDescribeSafety
+            // 
+            this.TBDescribeSafety.Location = new System.Drawing.Point(98, 36);
+            this.TBDescribeSafety.Name = "TBDescribeSafety";
+            this.TBDescribeSafety.Size = new System.Drawing.Size(173, 21);
+            this.TBDescribeSafety.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "说明：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BUTChange);
+            this.groupBox1.Controls.Add(this.BUTQuery);
+            this.groupBox1.Controls.Add(this.BUTFocus);
+            this.groupBox1.Controls.Add(this.BUTAdd);
+            this.groupBox1.Controls.Add(this.BUTShow);
+            this.groupBox1.Location = new System.Drawing.Point(0, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 47);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "工具栏";
+            // 
+            // BUTAdd
+            // 
+            this.BUTAdd.Location = new System.Drawing.Point(300, 18);
+            this.BUTAdd.Name = "BUTAdd";
+            this.BUTAdd.Size = new System.Drawing.Size(75, 23);
+            this.BUTAdd.TabIndex = 1;
+            this.BUTAdd.Text = "新增";
+            this.BUTAdd.UseVisualStyleBackColor = true;
+            this.BUTAdd.Click += new System.EventHandler(this.BUTAdd_Click);
+            // 
+            // BUTShow
+            // 
+            this.BUTShow.Location = new System.Drawing.Point(16, 18);
+            this.BUTShow.Name = "BUTShow";
+            this.BUTShow.Size = new System.Drawing.Size(75, 23);
+            this.BUTShow.TabIndex = 0;
+            this.BUTShow.Text = "显示";
+            this.BUTShow.UseVisualStyleBackColor = true;
+            this.BUTShow.Click += new System.EventHandler(this.BUTShow_Click);
+            // 
+            // IDofDGVTotal
+            // 
+            this.IDofDGVTotal.FillWeight = 1F;
+            this.IDofDGVTotal.HeaderText = "ID";
+            this.IDofDGVTotal.Name = "IDofDGVTotal";
+            // 
+            // NAMEofDGVTotal
+            // 
+            this.NAMEofDGVTotal.FillWeight = 1F;
+            this.NAMEofDGVTotal.HeaderText = "名称";
+            this.NAMEofDGVTotal.Name = "NAMEofDGVTotal";
+            // 
+            // BUTFocus
+            // 
+            this.BUTFocus.Location = new System.Drawing.Point(109, 18);
+            this.BUTFocus.Name = "BUTFocus";
+            this.BUTFocus.Size = new System.Drawing.Size(75, 23);
+            this.BUTFocus.TabIndex = 2;
+            this.BUTFocus.Text = "关注";
+            this.BUTFocus.UseVisualStyleBackColor = true;
+            this.BUTFocus.Click += new System.EventHandler(this.BUTFocus_Click);
+            // 
+            // BUTQuery
+            // 
+            this.BUTQuery.Location = new System.Drawing.Point(191, 18);
+            this.BUTQuery.Name = "BUTQuery";
+            this.BUTQuery.Size = new System.Drawing.Size(75, 23);
+            this.BUTQuery.TabIndex = 3;
+            this.BUTQuery.Text = "查询";
+            this.BUTQuery.UseVisualStyleBackColor = true;
+            this.BUTQuery.Click += new System.EventHandler(this.BUTQuery_Click);
+            // 
+            // BUTChange
+            // 
+            this.BUTChange.Location = new System.Drawing.Point(397, 18);
+            this.BUTChange.Name = "BUTChange";
+            this.BUTChange.Size = new System.Drawing.Size(75, 23);
+            this.BUTChange.TabIndex = 4;
+            this.BUTChange.Text = "修改";
+            this.BUTChange.UseVisualStyleBackColor = true;
+            this.BUTChange.Click += new System.EventHandler(this.BUTChange_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "MainForm";
             this.Text = "信息总览";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVTotal)).EndInit();
+            this.TCLTotal.ResumeLayout(false);
+            this.QualityTP.ResumeLayout(false);
+            this.QualityTP.PerformLayout();
+            this.SafetyTP.ResumeLayout(false);
+            this.SafetyTP.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView DGVTotal;
+        private System.Windows.Forms.TabControl TCLTotal;
+        private System.Windows.Forms.TabPage QualityTP;
+        private System.Windows.Forms.TabPage SafetyTP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBDescribeQuality;
+        private System.Windows.Forms.TextBox TBDescribeSafety;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BUTShow;
+        private System.Windows.Forms.Button BUTAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDofDGVTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NAMEofDGVTotal;
+        private System.Windows.Forms.Button BUTFocus;
+        private System.Windows.Forms.Button BUTQuery;
+        private System.Windows.Forms.Button BUTChange;
     }
 }
--- Info9DProject/MainForm.cs
+++ Info9DProject/MainForm.cs
@@ -7,14 +7,239 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using Autodesk.Navisworks.Api;
 
 namespace Info9DProject
 {
     public partial class MainForm : Form
     {
+        private const string sqlConnectInfo = "data source=192.168.0.69;Initial Catalog=ZYCT_Project;User ID=sa;PWD=sasa;";
+        private const string mainTableName = "BIM_9DMain";
+        private const string qualityTableName = "BIM_9DQualityInfo";
+        private const string safetyTableName = "BIM_9DSafetyInfo";
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void BUTShow_Click(object sender, EventArgs e)
+        {
+            DGVTotal.Rows.Clear();
+            /**操作数据库**/
+            SqlConnection con = new SqlConnection(sqlConnectInfo);
+            con.Open();
+            string sqlCmd = string.Format("SELECT ID,Name FROM {0}",mainTableName);
+            SqlCommand cmd = new SqlCommand(sqlCmd, con);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+            int a = DGVTotal.Rows.Count;
+            for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+            {
+                DGVTotal.Rows.Add();
+                for (int j = 0; j < dataSet.Tables[0].Columns.Count; j++)
+                {
+                    DGVTotal.Rows[i].Cells[j].Value = dataSet.Tables[0].Rows[i][j];
+                }
+            }
+            con.Close();
+        }
+
+        //查询数据库中是否存在该ID的记录，有的话选中当前窗口tab页的该行
+        private bool HasRecordInSqlTable(string strId)
+        {
+            //入库查询
+            SqlConnection con = new SqlConnection(sqlConnectInfo);
+            con.Open();
+            string cmdText = string.Format("SELECT * FROM {0} WHERE ID='{1}'",mainTableName,strId);
+            SqlCommand sqlCommand = new SqlCommand(cmdText, con);
+
+            if (sqlCommand.ExecuteScalar() == null)//查询数据库中是否有此记录
+                return false;
+            else
+            {
+                for (int i = 0; i < DGVTotal.Rows.Count; i++)
+                {
+                    if (DGVTotal.Rows[i].Cells[0].Value.ToString() == strId)
+                    {
+                        DGVTotal.Rows[i].Selected = true;//设置改行选中
+                        DGVTotal.CurrentCell = DGVTotal.Rows[i].Cells[0];//焦点到该行
+                    }
+                }
+            }                             
+                
+            con.Close();
+            return true;
+        }
+
+        private void BUTAdd_Click(object sender, EventArgs e)
+        {
+            //规定只能选择单个构件添加记录
+            Document doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
+            Selection selection = new Selection();
+            selection = doc.CurrentSelection.ToSelection();
+            ModelItemCollection modelItems = selection.ExplicitSelection;
+
+            if (modelItems.Count != 1)
+            {
+                MessageBox.Show("请只选择一个图元新增记录！");
+                return;
+            }
+            ModelItem modelItem = modelItems.First;
+            int nId = -1;
+            string strName = "";
+            try
+            {
+                nId = modelItem.PropertyCategories.FindPropertyByDisplayName("元素", "Id").Value.ToInt32();
+                strName = modelItem.PropertyCategories.FindPropertyByDisplayName("元素", "名称").Value.ToDisplayString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("查询错误" + ex.Message);
+                return;
+            }
+            string strId = nId.ToString();
+            //查询是否有记录，有的话聚焦并选择
+            if (HasRecordInSqlTable(strId))
+            {
+                MessageBox.Show("已有对应记录，显示在表中！", "提示");
+            }
+            else//添加记录
+            {
+                SqlConnection con = new SqlConnection(sqlConnectInfo);
+                con.Open();
+                string strTotalSqlText = string.Format("INSERT INTO {0} VALUES ('{1}','{2}')", mainTableName,strId, strName);
+                string qualityDescribeText = "";
+                if (TBDescribeQuality.Text.Trim() != string.Empty)
+                    qualityDescribeText = TBDescribeQuality.Text;
+                string strQualitySqlText = string.Format("INSERT INTO {0} VALUES ('{1}','{2}','{3}')", qualityTableName, strId, strName, qualityDescribeText);
+                string safetyDescribeText = "";
+                if (TBDescribeSafety.Text.Trim() != string.Empty)
+                    safetyDescribeText = TBDescribeSafety.Text;
+                string strSafetySqlText = string.Format("INSERT INTO {0} VALUES ('{1}','{2}','{3}')", safetyTableName, strId, strName, safetyDescribeText);
+                SqlCommand cmdTotalSqlText = new SqlCommand(strTotalSqlText, con);
+                SqlCommand cmdQuality = new SqlCommand(strQualitySqlText, con);
+                SqlCommand cmdSafety = new SqlCommand(strSafetySqlText, con);
+                try
+                {
+                    cmdTotalSqlText.ExecuteNonQuery();
+                    cmdQuality.ExecuteNonQuery();
+                    cmdSafety.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("提交错误：" + ex.Message);
+                    return;
+                }
+                
+                BUTShow_Click(sender, e);//刷新总表
+                con.Close();
+            }
+        }
+
+        private void DGVTotal_CellClick(object sender, DataGridViewCellEventArgs e)//刷新tabControl的表
+        {
+            DataGridViewRow selectedRow = DGVTotal.SelectedRows[0];
+            string sId = selectedRow.Cells[0].Value.ToString();
+            SqlConnection con = new SqlConnection(sqlConnectInfo);
+            con.Open();
+            string strQualitySqlText = string.Format("SELECT Describe FROM {0} WHERE ID = {1}", qualityTableName, sId);
+            string strSafetySqlText = string.Format("SELECT Describe FROM {0} WHERE ID={1}", safetyTableName, sId);
+            SqlCommand cmdQuality = new SqlCommand(strQualitySqlText, con);
+            SqlCommand cmdSafety = new SqlCommand(strSafetySqlText, con);
+            SqlDataReader reader;
+            string strDescribeQuality = "";
+            string strDescribeSafety = "";
+            try
+            {
+                reader = cmdQuality.ExecuteReader();
+                reader.Read();//只读唯一的一条记录
+                if (!reader.IsDBNull(reader.GetOrdinal("Describe")))//db中有数据
+                    strDescribeQuality = reader.GetString(reader.GetOrdinal("Describe"));
+                reader.Close();//如需要开启另一个reader，必须关闭之前的reader
+                reader = cmdSafety.ExecuteReader();
+                reader.Read();
+                if (!reader.IsDBNull(reader.GetOrdinal("Describe")))//db中有数据
+                    strDescribeSafety = reader.GetString(reader.GetOrdinal("Describe"));
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("数据库数据加载错误信息:\n" + ex.Message);
+                return;
+            }
+                       
+            TBDescribeQuality.Text = strDescribeQuality;                      
+            TBDescribeSafety.Text = strDescribeSafety;
+            con.Close();
+        }
+
+        private void BUTFocus_Click(object sender, EventArgs e)
+        {
+            Document Doc = Autodesk.Navisworks.Api.Application.ActiveDocument;                        
+            List<string> listId = new List<string>();
+            if (DGVTotal.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("未选中行！");
+                return;
+            }
+            for (int i = 0; i < DGVTotal.SelectedRows.Count; i++)
+            {
+                listId.Add(DGVTotal.SelectedRows[i].Cells[0].Value.ToString());
+            }
+
+            ModelItemCollection itemsCol = new ModelItemCollection();
+            //元素ID是int32类型，所以Displaystring找不到
+            for (int i = 0; i < DGVTotal.SelectedRows.Count; i++)
+            {
+                Search search = new Search();
+                search.Selection.SelectAll();
+                search.SearchConditions.Add(SearchCondition.HasPropertyByDisplayName("元素", "Id").EqualValue(VariantData.FromInt32(Int32.Parse(listId[i]))));
+                ModelItemCollection items = search.FindAll(Doc, true);
+                itemsCol.Add(items.First);
+            }
+            
+                     
+            Doc.CurrentSelection.Clear();
+            Doc.CurrentSelection.CopyFrom(itemsCol);
+            if (itemsCol.Count > 0)
+            {
+                MessageBox.Show("已在视图中选中该元素！", "提示");
+            }
+            
+        }
+
+        private void BUTQuery_Click(object sender, EventArgs e)
+        {
+            Document Doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
+            ModelItemCollection modelItems = Doc.CurrentSelection.SelectedItems;
+            if (modelItems.Count != 1)
+            {
+                MessageBox.Show("请选择一个构件查询", "提示");
+                return;
+            }
+            ModelItem modelItem = modelItems.First;
+            int nId = -1;
+            try
+            {
+                nId = modelItem.PropertyCategories.FindPropertyByDisplayName("元素", "Id").Value.ToInt32();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("查询错误" + ex.Message);
+                return;
+            }
+            string strId = nId.ToString();
+            if (!HasRecordInSqlTable(strId))//查询并聚焦
+                MessageBox.Show("该构件未查询到信息!", "信息");
+        }
+
+        private void BUTChange_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
